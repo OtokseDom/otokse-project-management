f0e67d5f6b2cfd4b92b9c3eb3b7dec43b205ceae
Dominic Escoto <128564959+OtokseDom@users.noreply.github.com>
2025-11-22 20:23:18 +0400

    Update(Section Card): Add tooltip prop

--- frontend/src/components/chart/section-card.jsx
+++ frontend/src/components/chart/section-card.jsx
@@ -1,10 +1,11 @@
 import { Badge } from "@/components/ui/badge";
 import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { useLoadContext } from "@/contexts/LoadContextProvider";
-import { TrendingDown, TrendingUp } from "lucide-react";
+import { Info, TrendingDown, TrendingUp } from "lucide-react";
 import { Skeleton } from "../ui/skeleton";
+import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
 
-export function SectionCard({ description, value, showBadge = true, percentage, upward = true, insight, footer, variant = "" }) {
+export function SectionCard({ description, value, tooltip = "", showBadge = true, percentage, upward = true, insight, footer, variant = "" }) {
 	const { loading } = useLoadContext();
 	return (
 		// <div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4">
@@ -18,6 +19,14 @@ export function SectionCard({ description, value, showBadge = true, percentage, 
 							<Badge variant="outline" className="gap-1">
 								{upward ? <TrendingUp size={12} /> : <TrendingDown size={12} />} {percentage ? percentage : "N/A"}
 							</Badge>
+						)}
+						{tooltip !== "" && (
+							<Tooltip>
+								<TooltipTrigger>
+									<Info size={16} className="text-muted-foreground hover:cursor-help" />
+								</TooltipTrigger>
+								<TooltipContent>{tooltip}</TooltipContent>
+							</Tooltip>
 						)}
 					</div>
 					<CardTitle className="text-3xl font-semibold tabular-nums">
