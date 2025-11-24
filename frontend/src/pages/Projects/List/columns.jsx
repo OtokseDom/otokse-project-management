abb804d764f2737fcfb324006a60deea713e2be7
Dominic Escoto <128564959+OtokseDom@users.noreply.github.com>
2025-11-24 19:47:03 +0400

    Update(Project column): Action buttons to be visible without dialog

--- frontend/src/pages/Projects/List/columns.jsx
+++ frontend/src/pages/Projects/List/columns.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { ArrowUpDown } from "lucide-react";
+import { ArrowUpDown, Edit, Trash2Icon } from "lucide-react";
 import { MoreHorizontal } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
@@ -275,28 +275,30 @@ export const columnsProject = ({ handleDelete, setIsOpen, setUpdateData, dialogO
 			cell: ({ row }) => {
 				const project = row.original;
 				return (
-					<DropdownMenu modal={false}>
-						<DropdownMenuTrigger asChild>
-							<Button variant="ghost" className="h-8 w-8 p-0">
-								<span className="sr-only">Open menu</span>
-								<MoreHorizontal className="h-4 w-4" />
-							</Button>
-						</DropdownMenuTrigger>
-						<DropdownMenuContent align="end">
-							<DropdownMenuItem className="cursor-pointer" onClick={() => handleUpdateProject(project)}>
-								Update Project
-							</DropdownMenuItem>
-							<DropdownMenuItem
-								className="cursor-pointer"
-								onClick={(e) => {
-									e.stopPropagation();
-									openDialog(project);
-								}}
-							>
-								Delete Project
-							</DropdownMenuItem>
-						</DropdownMenuContent>
-					</DropdownMenu>
+					<div className="flex justify-center items-center">
+						<Button
+							variant="ghost"
+							title="Update task"
+							className="h-8 w-8 p-0 cursor-pointer pointer-events-auto"
+							onClick={(e) => {
+								e.stopPropagation();
+								handleUpdateProject(project);
+							}}
+						>
+							<Edit size={16} />
+						</Button>
+						<Button
+							variant="ghost"
+							title="Delete task"
+							className="h-8 w-8 p-0 cursor-pointer pointer-events-auto"
+							onClick={(e) => {
+								e.stopPropagation();
+								openDialog(project);
+							}}
+						>
+							<Trash2Icon className="text-destructive" />
+						</Button>
+					</div>
 				);
 			},
 		});
