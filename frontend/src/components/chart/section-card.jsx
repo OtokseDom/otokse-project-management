b9a38ebb6a15e51acdd4d23bbd14ed7ac6154e44
Dominic Escoto <imjohndominic08@gmail.com>
2025-11-25 21:50:33 +0400

    Update(Section card component): Use dashboardReportsLoading and userreportsLoading state

--- frontend/src/components/chart/section-card.jsx
+++ frontend/src/components/chart/section-card.jsx
@@ -1,12 +1,14 @@
 import { Badge } from "@/components/ui/badge";
-import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-import { useLoadContext } from "@/contexts/LoadContextProvider";
+import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Info, TrendingDown, TrendingUp } from "lucide-react";
 import { Skeleton } from "../ui/skeleton";
 import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
+import { useDashboardStore } from "@/store/dashboard/dashboardStore";
+import { useUserStore } from "@/store/user/userStore";
 
 export function SectionCard({ description, value, tooltip = "", showBadge = true, percentage, upward = true, insight, footer, variant = "" }) {
-	const { loading } = useLoadContext();
+	const { dashboardReportsLoading } = useDashboardStore();
+	const { userReportsLoading } = useUserStore();
 	return (
 		// <div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4">
 		<div className="w-full">
@@ -30,7 +32,7 @@ export function SectionCard({ description, value, tooltip = "", showBadge = true
 						)}
 					</div>
 					<CardTitle className="text-3xl font-semibold tabular-nums">
-						{loading ? (
+						{dashboardReportsLoading || userReportsLoading ? (
 							<div className="flex flex-col gap-2 items-center justify-center h-full w-full">
 								<Skeleton className=" w-full h-4" />
 								<Skeleton className=" w-full h-4" />
