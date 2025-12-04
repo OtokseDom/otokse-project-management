2f85bc613cafe4401074bb0907514f5fef0cb30a
OtokseDom <imjohndominic08@gmail.com>
2025-12-04 20:55:38 +0400

    Add(Task Position Requests): with update definition

---
+++ backend/app/Http/Requests/UpdateTaskPositionRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateTaskPositionRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'task_id' => 'required|integer|exists:tasks,id',
+            'context' => 'required|string|in:project,all_projects,kanban_status',
+            'context_id' => 'nullable|integer',
+            'position' => 'required|integer|min:1',
+        ];
+    }
+}
