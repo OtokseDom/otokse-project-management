03e8cbd07935de6d8bb5f826a83987f9d7e361f1
OtokseDom <imjohndominic08@gmail.com>
2025-12-04 20:55:55 +0400

    Add(Task postition resource)

---
+++ backend/app/Http/Resources/TaskPositionResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TaskPositionResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'task_id' => $this->task_id,
+            'context' => $this->context,
+            'context_id' => $this->context_id,
+            'position' => $this->position,
+            'organization_id' => $this->organization_id,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
