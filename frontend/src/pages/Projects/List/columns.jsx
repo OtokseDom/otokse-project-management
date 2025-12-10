57bbf6826635f52880b905ea437830c7bcec9d6f
Dominic Escoto <128564959+OtokseDom@users.noreply.github.com>
2025-12-10 21:10:24 +0400

    Update(Project columns): Use delete dialog for deleting

--- frontend/src/pages/Projects/List/columns.jsx
+++ frontend/src/pages/Projects/List/columns.jsx
@@ -1,42 +1,16 @@
 "use client";
 import { ArrowUpDown, Edit, ListTodo, Trash2Icon } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from "@/components/ui/dialog";
-import { useEffect, useMemo, useState } from "react";
+import { useMemo } from "react";
 import { useAuthContext } from "@/contexts/AuthContextProvider";
 import { format } from "date-fns";
-import { useToast } from "@/contexts/ToastContextProvider";
-import axiosClient from "@/axios.client";
-import { API } from "@/constants/api";
 import { statusColors, priorityColors } from "@/utils/taskHelpers";
 import { useProjectsStore } from "@/store/projects/projectsStore";
 import { Link } from "react-router-dom";
-export const columnsProject = ({ handleDelete, setIsOpen, setUpdateData, dialogOpen, setDialogOpen }) => {
-	const { setSelectedProject, projects, projectsLoading, setProjectsLoading } = useProjectsStore();
-	const showToast = useToast();
+import DeleteDialog from "./deleteDialog";
+export const columnsProject = ({ setIsOpen, setUpdateData, dialogOpen, setDialogOpen, checkHasRelation, hasRelation, selectedProjectId }) => {
+	const { setSelectedProject, projects } = useProjectsStore();
 	const { user } = useAuthContext(); // Get authenticated user details
-	const [selectedProjectId, setSelectedProjectId] = useState(null);
-	const [hasRelation, setHasRelation] = useState(false);
-
-	const openDialog = async (project = {}) => {
-		setProjectsLoading(true);
-		setTimeout(() => {
-			setDialogOpen(true);
-		}, 100);
-		setSelectedProjectId(project.id);
-		try {
-			const hasRelationResponse = await axiosClient.post(API().relation_check("project", project.id));
-			setHasRelation(hasRelationResponse?.data?.data?.exists);
-		} catch (e) {
-			showToast("Failed!", e.response?.data?.message, 3000, "fail");
-			if (e.message !== "Request aborted") console.error("Error fetching data:", e.message);
-		} finally {
-			setProjectsLoading(false);
-		}
-	};
-	useEffect(() => {
-		if (!dialogOpen) setHasRelation(false);
-	}, [dialogOpen]);
 
 	const handleUpdateProject = (project) => {
 		setTimeout(() => {
@@ -117,38 +91,6 @@ export const columnsProject = ({ handleDelete, setIsOpen, setUpdateData, dialogO
 					);
 				},
 			},
-			// {
-			// 	id: "target_date",
-			// 	accessorKey: "target_date",
-			// 	header: ({ column }) => (
-			// 		<button className="flex" onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
-			// 			Target Date <ArrowUpDown className="ml-2 h-4 w-4" />
-			// 		</button>
-			// 	),
-			// 	// Keep raw value for sorting
-			// 	accessorFn: (row) => row.target_date,
-			// 	// Use cell renderer to format for display
-			// 	cell: ({ row }) => {
-			// 		const date = row.original.target_date;
-			// 		return date ? format(new Date(date), "MMM-dd yyyy") : "";
-			// 	},
-			// },
-			// {
-			// 	id: "estimated_date",
-			// 	accessorKey: "estimated_date",
-			// 	header: ({ column }) => (
-			// 		<button className="flex" onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
-			// 			Estimated Date <ArrowUpDown className="ml-2 h-4 w-4" />
-			// 		</button>
-			// 	),
-			// 	// Keep raw value for sorting
-			// 	accessorFn: (row) => row.estimated_date,
-			// 	// Use cell renderer to format for display
-			// 	cell: ({ row }) => {
-			// 		const date = row.original.estimated_date;
-			// 		return date ? format(new Date(date), "MMM-dd yyyy") : "";
-			// 	},
-			// },
 			{
 				id: "start date",
 				accessorKey: "start_date",
@@ -304,7 +246,7 @@ export const columnsProject = ({ handleDelete, setIsOpen, setUpdateData, dialogO
 									className="h-8 w-8 p-0 cursor-pointer pointer-events-auto"
 									onClick={(e) => {
 										e.stopPropagation();
-										openDialog(project);
+										checkHasRelation(project);
 									}}
 								>
 									<Trash2Icon className="text-destructive" />
@@ -318,41 +260,8 @@ export const columnsProject = ({ handleDelete, setIsOpen, setUpdateData, dialogO
 		return baseColumns;
 	}, [baseColumns, user?.data?.role]);
 
-	const dialog = (
-		<Dialog open={dialogOpen} onOpenChange={setDialogOpen} modal={true}>
-			<DialogContent>
-				<DialogHeader>
-					<DialogTitle>{hasRelation ? <span className="text-yellow-800">Warning</span> : "Are you absolutely sure?"}</DialogTitle>
-					<DialogDescription>{!hasRelation && "This action cannot be undone."}</DialogDescription>
-				</DialogHeader>
-				<div className="ml-4 text-base">
-					{hasRelation && (
-						<>
-							<span className="text-yellow-800">Project cannot be deleted because it has assigned tasks.</span>
-						</>
-					)}
-				</div>
-				<DialogFooter>
-					<DialogClose asChild>
-						<Button type="button" variant="secondary">
-							Close
-						</Button>
-					</DialogClose>
-					{!hasRelation && (
-						<Button
-							disabled={projectsLoading}
-							onClick={() => {
-								handleDelete(selectedProjectId);
-								setDialogOpen(false);
-							}}
-						>
-							Yes, delete
-						</Button>
-					)}
-				</DialogFooter>
-			</DialogContent>
-		</Dialog>
-	);
-	// Return columns and dialog as an object (consumer should use columns and render dialog outside table)
-	return { columnsProject: columnsWithActions, dialog };
+	return {
+		columnsProject: columnsWithActions,
+		dialog: <DeleteDialog dialogOpen={dialogOpen} setDialogOpen={setDialogOpen} hasRelation={hasRelation} selectedProjectId={selectedProjectId} />,
+	};
 };
