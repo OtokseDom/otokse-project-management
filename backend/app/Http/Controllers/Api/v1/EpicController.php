1222fa3fcd0774f9f1e2eb7be9199ac16928d447
Dominic Escoto <128564959+OtokseDom@users.noreply.github.com>
2025-12-16 22:40:07 +0400

    Update(Epic Controller): Create api functions

--- backend/app/Http/Controllers/Api/v1/EpicController.php
+++ backend/app/Http/Controllers/Api/v1/EpicController.php
@@ -3,47 +3,84 @@
 namespace App\Http\Controllers\Api\v1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreEpicRequest;
+use App\Http\Requests\UpdateEpicRequest;
+use App\Http\Resources\EpicResource;
+use App\Models\Epic;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class EpicController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    protected Epic $epic;
+    protected $userData;
+    public function __construct(Epic $epic)
+    {
+        $this->epic = $epic;
+        $this->userData = Auth::user();
+    }
     public function index()
     {
-        //
+        $epics = $this->epic->getProjects($this->userData->organization_id);
+        $data = [
+            "epics" => $epics,
+        ];
+        return apiResponse($data, 'Epics fetched successfully');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
+    public function store(StoreEpicRequest $request)
     {
-        //
+        // TODO: storeEpic model function
+        $new = $this->epic->storeEpic($request, $this->userData);
+        if ($new === "not found") {
+            return apiResponse(null, 'Organization not found.', false, 404);
+        }
+        if (!$new) {
+            return apiResponse(null, 'Epic creation failed', false, 404);
+        }
+        $data = [
+            "epic" => new EpicResource($new['epic']),
+        ];
+        return apiResponse($data, 'Epic created successfully', true, 201);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
+    public function show(Epic $epic)
     {
-        //
+        // TODO: showEpic model function
+        $details = $this->epic->showEpic($this->userData->organization_id, $epic->id);
+        if (!$details) {
+            return apiResponse(null, 'Epic not found', false, 404);
+        }
+        return apiResponse(new EpicResource($details), 'Epic details fetched successfully');
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
+    public function update(UpdateEpicRequest $request, Epic $epic)
     {
-        //
+        // TODO: updateEpic model function
+        $updated = $this->epic->updateEpic($request, $epic, $this->userData);
+        if ($updated === "not found") {
+            return apiResponse(null, 'Epic not found.', false, 404);
+        }
+        if (!$updated) {
+            return apiResponse(null, 'Failed to update epic.', false, 500);
+        }
+        $epic->load(['status:id,name,color']);
+        return apiResponse(new EpicResource($epic), 'Epic updated successfully');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
+    public function destroy(Epic $epic)
     {
-        //
+        // TODO: deleteEpic model function
+        $result = $this->epic->deleteEpic($epic, $this->userData);
+        if ($result === "not found") {
+            return apiResponse(null, 'Epic not found.', false, 404);
+        }
+        if ($result === false) {
+            return apiResponse(null, 'Epic cannot be deleted because they have assigned projects.', false, 400);
+        }
+        if ($result === null) {
+            return apiResponse(null, 'Failed to delete epic.', false, 500);
+        }
+        return apiResponse('', 'Epic deleted successfully');
     }
 }
