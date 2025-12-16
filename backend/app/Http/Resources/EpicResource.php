a27cdf2af011a9e7297a85bdadf8cf90d390387a
Dominic Escoto <128564959+OtokseDom@users.noreply.github.com>
2025-12-16 22:40:27 +0400

    Update(Epic Resource): Populate data

--- backend/app/Http/Resources/EpicResource.php
+++ backend/app/Http/Resources/EpicResource.php
@@ -14,6 +14,26 @@ class EpicResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id'             => $this->id,
+            'organization_id' => $this->organization_id,
+            'status_id' => $this->status_id,
+            'title'          => $this->title,
+            'owner_id' => $this->owner_id,
+            'slug'          => $this->slug,
+            'description'    => $this->description,
+            'start_date'     => $this->start_date,
+            'end_date'       => $this->end_date,
+            'priority'       => $this->priority,
+            'remarks'        => $this->remarks,
+            'created_at'     => $this->created_at->format('Y-m-d H:i:s'),
+            'updated_at'     => $this->updated_at->format('Y-m-d H:i:s'),
+            'status' => $this->whenLoaded('status', function () {
+                return $this->status ? [
+                    'name' => $this->status->name,
+                    'color' => $this->status->color
+                ] : null;
+            }),
+        ];
     }
 }
