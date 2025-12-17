eb440fa0b650b1f36eab842b9699fbbadb197df1
Dominic Escoto <128564959+OtokseDom@users.noreply.github.com>
2025-12-17 21:33:50 +0400

    Add(TaskHelpers): fetchEpics

--- frontend/src/utils/taskHelpers.js
+++ frontend/src/utils/taskHelpers.js
@@ -3,6 +3,7 @@ import axiosClient from "@/axios.client";
 import { API } from "@/constants/api";
 import { useCategoriesStore } from "@/store/categories/categoriesStore";
 import { useDashboardStore } from "@/store/dashboard/dashboardStore";
+import { useEpicsStore } from "@/store/epics/epicsStore";
 import { useKanbanColumnsStore } from "@/store/kanbanColumns/kanbanColumnsStore";
 import { useProjectsStore } from "@/store/projects/projectsStore";
 import { useTaskDiscussionsStore } from "@/store/taskDiscussions/taskDiscussionsStore";
@@ -10,12 +11,12 @@ import { useTasksStore } from "@/store/tasks/tasksStore";
 import { useTaskStatusesStore } from "@/store/taskStatuses/taskStatusesStore";
 import { useUserStore } from "@/store/user/userStore";
 import { useUsersStore } from "@/store/users/usersStore";
-import { useEffect, useState } from "react";
 
 export const useTaskHelpers = () => {
 	const { projectFilter, setProjectFilter, userFilter, setUserFilter, setReports, setDashboardReportsLoading } = useDashboardStore();
 	const { setTasks, setTaskHistory, setOptions, setSelectedUser, setTasksLoading } = useTasksStore();
 	const { setTaskDiscussions, setTaskDiscussionsLoading } = useTaskDiscussionsStore();
+	const { setEpics, setSelectedEpic, setEpicsLoading } = useEpicsStore();
 	const { setProjects, setSelectedProject, setProjectsLoading } = useProjectsStore();
 	const { setUsersLoading, setUsers } = useUsersStore();
 	const { setCategories, setCategoriesLoading } = useCategoriesStore();
@@ -47,6 +48,27 @@ export const useTaskHelpers = () => {
 			if (e.message !== "Request aborted") console.error("Error fetching data:", e.message);
 		} finally {
 			setTaskDiscussionsLoading(false);
+		}
+	};
+
+	const fetchEpics = async () => {
+		setEpicsLoading(true);
+		try {
+			const res = await axiosClient.get(API().epic());
+			setEpics(res?.data?.data?.epics);
+			setKanbanColumns(res?.data?.data?.kanbanColumns);
+			setSelectedEpic(res?.data?.data?.epics[res?.data?.data?.epics?.length - 1]);
+			if (res.data.data.epics.length !== epicFilter.length || res.data.data.epics.length !== profileEpicFilter.length) {
+				const mappedEpics = res.data.data.epics.map((epic) => ({ value: epic.id, label: epic.title }));
+				// Used in user profile
+				// setProfileEpicFilter(mappedEpics);
+				// Used in dashboard
+				setEpicFilter(mappedEpics);
+			}
+		} catch (e) {
+			if (e.message !== "Request aborted") console.error("Error fetching data:", e.message);
+		} finally {
+			setProjectsLoading(false);
 		}
 	};
 
