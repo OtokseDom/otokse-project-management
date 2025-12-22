6411e9815b7b34c6b624860ea3ad1da277a6f342
Dominic Escoto <128564959+OtokseDom@users.noreply.github.com>
2025-12-22 21:25:00 +0400

    Add(Epic Show): index placeholder

---
+++ frontend/src/pages/Epics/Show/index.jsx
@@ -0,0 +1,102 @@
+import { Link, useParams } from "react-router-dom";
+import { ArrowLeft, Plus, Rows3, Table } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { useEffect, useState } from "react";
+import axiosClient from "@/axios.client";
+import { columnsTask } from "@/pages/Tasks/List/datatable/columns";
+import { DataTableTasks } from "@/pages/Tasks/List/datatable/data-table";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { PieChartDonut } from "@/components/chart/pie-chart-donut";
+import GalaxyProfileBanner from "@/components/design/galaxy";
+import { AreaChartGradient } from "@/components/chart/area-chart-gradient";
+import { RadarChartGridFilled } from "@/components/chart/radar-chart-grid-filled";
+import { ChartLineLabel } from "@/components/chart/line-chart-label";
+import { ChartBarMultiple } from "@/components/chart/bar-chart-multiple";
+import { ChartBarLabel } from "@/components/chart/bar-chart-label";
+import UserDetails from "@/pages/Users/Show/details";
+import { SectionCard } from "@/components/chart/section-card";
+import FilterForm from "@/components/form/filter-form";
+import FilterTags from "@/components/form/FilterTags";
+import { API } from "@/constants/api";
+import GalaxyProgressBar from "@/components/design/GalaxyProgressBar";
+import { flattenTasks, getProfileProjectProgress, useTaskHelpers } from "@/utils/taskHelpers";
+import { useUsersStore } from "@/store/users/usersStore";
+import { useProjectsStore } from "@/store/projects/projectsStore";
+import { useCategoriesStore } from "@/store/categories/categoriesStore";
+import { useTasksStore } from "@/store/tasks/tasksStore";
+import { useUserStore } from "@/store/user/userStore";
+import UserForm from "../form";
+import { useTaskStatusesStore } from "@/store/taskStatuses/taskStatusesStore";
+import { useDashboardStore } from "@/store/dashboard/dashboardStore";
+import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import TaskForm from "../../Tasks/form.jsx";
+import History from "@/components/task/History";
+import Relations from "@/components/task/Relations";
+import Tabs from "@/components/task/Tabs";
+import { TaskDiscussions } from "@/components/task/Discussion";
+import GridList from "@/pages/Tasks/List/grid/gridList";
+import { Progress } from "@/components/ui/progress";
+
+export default function EpicDetails() {
+	const { id } = useParams();
+
+	return (
+		<div className="flex flex-col w-screen md:w-full container p-5 md:p-0 sm:text-sm -mt-10">
+			{/* <div
+				className={`fixed inset-0 bg-black bg-opacity-60 backdrop-blur-sm z-40 transition-opacity duration-300 pointer-events-none ${
+					isOpen || isOpenUser || isOpenFilter || dialogOpen || deleteDialogOpen ? "opacity-100" : "opacity-0"
+				}`}
+				aria-hidden="true"
+			/> */}
+
+			{/* Back button */}
+			<Link to="/users">
+				<Button variant="ghost" className="flex items-center">
+					<ArrowLeft />
+				</Button>
+			</Link>
+
+			{/* Update user Form Sheet */}
+			{/* <Sheet open={isOpenUser} onOpenChange={setIsOpenUser} modal={false}>
+				<SheetContent side="right" className="overflow-y-auto w-[400px] sm:w-[540px]">
+					<SheetHeader>
+						<SheetTitle>Update User</SheetTitle>
+						<SheetDescription className="sr-only">Navigate through the app using the options below.</SheetDescription>
+					</SheetHeader>
+					<UserForm setIsOpen={setIsOpenUser} updateData={user} userProfileId={id} />
+				</SheetContent>
+			</Sheet> */}
+
+			{/* Main Content Grid */}
+			<div className="w-full grid grid-cols-1 md:grid-cols-12 gap-4 md:gap-4 auto-rows-auto mt-4">
+				{/* Filter Section */}
+				<div className="md:col-span-12">
+					<div className="flex flex-wrap justify-start items-center gap-4">
+						Epic profile here
+						{/* <Dialog modal={false} open={isOpenFilter} onOpenChange={setIsOpenFilter}>
+							<DialogTrigger asChild>{!userReportsLoading && <Button variant="default">Filter</Button>}</DialogTrigger>
+							<DialogContent>
+								<DialogHeader>
+									<DialogTitle>Select filter</DialogTitle>
+									<DialogDescription>Apply available filters to view specific reports</DialogDescription>
+								</DialogHeader>
+								<FilterForm
+									setIsOpen={setIsOpenFilter}
+									setReports={setUserReports}
+									filters={profileFilters}
+									setFilters={setProfileFilters}
+									projects={projectFilter}
+									selectedProjects={profileSelectedProjects}
+									setSelectedProjects={setProfileSelectedProjects}
+									userId={id}
+								/>
+							</DialogContent>
+						</Dialog>
+						<FilterTags filters={profileFilters.display} onRemove={handleRemoveFilter} /> */}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
