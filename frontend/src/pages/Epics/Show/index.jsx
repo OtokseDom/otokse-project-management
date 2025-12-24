0761eb7ea40e93a4ada73d102e6c2a757f9eb009
Dominic Escoto <128564959+OtokseDom@users.noreply.github.com>
2025-12-24 23:35:30 +0400

    Update(Epic show): Improve layout

--- frontend/src/pages/Epics/Show/index.jsx
+++ frontend/src/pages/Epics/Show/index.jsx
@@ -71,24 +71,120 @@ export default function EpicDetails() {
 			{/* Main Content Grid */}
 			<div className="w-full grid grid-cols-1 md:grid-cols-12 gap-2 auto-rows-auto mt-4">
 				<div className="col-span-12 mb-4">
-					<h1 className="flex items-center gap-4 font-bold text-3xl">
-						<Flag /> Some of the most epic title in the history
+					<h1 className="flex items-start md:items-center gap-4 font-bold text-3xl">
+						<Flag className="hidden md:block" size={24} /> Some of the most epic title in the history
 					</h1>
 					{/* <p>View list of all epics</p> */}
 				</div>
-				{/* Filter Section */}
-				<div className="md:col-span-8 h-fit bg-card text-card-foreground border border-border rounded-xl container p-6">
-					<div className="flex w-full font-bold text-lg mb-4">Details</div>
-					<div className="flex flex-wrap justify-start items-center gap-4">Long description of epic here. Can be Markdown in future.</div>
+				{/* Main Panel */}
+				<div className="col-span-12 md:col-span-8 h-fit flex flex-col gap-2">
+					{/* Epic Details - mobile view */}
+					<div className="block md:hidden bg-card text-card-foreground border border-border rounded-xl container p-6">
+						<div className="flex w-full font-bold text-lg mb-4">Details</div>
+						<div className="flex flex-wrap justify-start items-center gap-4">Long description of epic here. Can be Markdown in future.</div>
+						<div className="flex flex-wrap justify-start items-center gap-4">Long description of epic here. Can be Markdown in future.</div>
+						<div className="flex flex-wrap justify-start items-center gap-4">Long description of epic here. Can be Markdown in future.</div>
+						<div className="flex flex-wrap justify-start items-center gap-4">Long description of epic here. Can be Markdown in future.</div>
+					</div>
+					{/* Epic Projects */}
+					<div className="bg-card text-card-foreground border border-border rounded-xl container p-6">
+						<div className="flex w-full font-bold text-lg mb-4">Projects</div>
+						<div className="flex overflow-auto justify-start items-start gap-2 pb-2">
+							<Button variant="secondary" className="min-w-fit">
+								Long ass Project name with uninterruptability. Niyugon mo yan. Ata ka na
+							</Button>
+							<Button variant="secondary" className="w-fit md:w-full">
+								Project 2
+							</Button>
+							<Button variant="secondary" className="w-fit md:w-full">
+								Project 3
+							</Button>
+							<Button variant="secondary" className="w-fit md:w-full">
+								Project 4
+							</Button>
+						</div>
+					</div>
+					{/* Epic Project-Tasks */}
+					{/* TODO: Bring grid list and item here */}
+					<div className="bg-card text-card-foreground max-h-screen overflow-auto border border-border rounded-xl container p-6">
+						<div className="flex w-full font-bold text-lg mb-4">Tasks</div>
+						<div className="flex flex-col justify-start items-start gap-2 pb-2">
+							<Button variant="secondary" className="min-w-fit">
+								Long ass Task name with uninterruptability. Niyugon mo yan. Ata ka na
+							</Button>
+							<Button variant="secondary" className="w-fit md:w-full">
+								Task 2
+							</Button>
+							<Button variant="secondary" className="w-fit md:w-full">
+								Task 3
+							</Button>
+							<Button variant="secondary" className="w-fit md:w-full">
+								Task 4
+							</Button>
+							<Button variant="secondary" className="w-fit md:w-full">
+								Task 4
+							</Button>
+							<Button variant="secondary" className="w-fit md:w-full">
+								Long ass Task name with uninterruptability. Niyugon mo yan. Ata ka na Task 4
+							</Button>
+							<Button variant="secondary" className="w-fit md:w-full">
+								Long ass Task name with uninterruptability. Niyugon mo yan. Ata ka na Task 4
+							</Button>
+							<Button variant="secondary" className="w-fit md:w-full">
+								Long ass Task name with uninterruptability. Niyugon mo yan. Ata ka na Task 4
+							</Button>
+							<Button variant="secondary" className="w-fit md:w-full">
+								Long ass Task name with uninterruptability. Niyugon mo yan. Ata ka na Task 4
+							</Button>
+							<Button variant="secondary" className="w-fit md:w-full">
+								Long ass Task name with uninterruptability. Niyugon mo yan. Ata ka na Task 4
+							</Button>
+							<Button variant="secondary" className="w-fit md:w-full">
+								Long ass Task name with uninterruptability. Niyugon mo yan. Ata ka na Task 4
+							</Button>
+							<Button variant="secondary" className="w-fit md:w-full">
+								Long ass Task name with uninterruptability. Niyugon mo yan. Ata ka na Task 4
+							</Button>
+							<Button variant="secondary" className="w-fit md:w-full">
+								Long ass Task name with uninterruptability. Niyugon mo yan. Ata ka na Task 4
+							</Button>
+							<Button variant="secondary" className="w-fit md:w-full">
+								Long ass Task name with uninterruptability. Niyugon mo yan. Ata ka na Task 4
+							</Button>
+							<Button variant="secondary" className="w-fit md:w-full">
+								Long ass Task name with uninterruptability. Niyugon mo yan. Ata ka na Task 4
+							</Button>
+							<Button variant="secondary" className="w-fit md:w-full">
+								Long ass Task name with uninterruptability. Niyugon mo yan. Ata ka na Task 4
+							</Button>
+							<Button variant="secondary" className="w-fit md:w-full">
+								Long ass Task name with uninterruptability. Niyugon mo yan. Ata ka na Task 4
+							</Button>
+							<Button variant="secondary" className="w-fit md:w-full">
+								Long ass Task name with uninterruptability. Niyugon mo yan. Ata ka na Task 4
+							</Button>
+							<Button variant="secondary" className="w-fit md:w-full">
+								Long ass Task name with uninterruptability. Niyugon mo yan. Ata ka na Task 4
+							</Button>
+						</div>
+					</div>
 				</div>
-				{/* Epic Details */}
-				<div className="md:col-span-4 h-fit bg-card text-card-foreground border border-border rounded-xl container p-6">
-					<div className="flex w-full font-bold text-lg mb-4">Projects</div>
-					<div className="flex flex-col justify-start items-start gap-4">
-						<p>Project 1</p>
-						<p>Project 2</p>
-						<p>Project 3</p>
-						<p>Project 4</p>
+				{/* Side panel */}
+				<div className="col-span-12 md:col-span-4 h-fit flex flex-col gap-2">
+					{/* Epic Details */}
+					<div className="hidden md:block bg-card text-card-foreground border border-border rounded-xl container p-6">
+						<div className="flex w-full font-bold text-lg mb-4">Details</div>
+						<div className="flex flex-wrap justify-start items-center gap-4">Long description of epic here. Can be Markdown in future.</div>
+						<div className="flex flex-wrap justify-start items-center gap-4">Long description of epic here. Can be Markdown in future.</div>
+						<div className="flex flex-wrap justify-start items-center gap-4">Long description of epic here. Can be Markdown in future.</div>
+						<div className="flex flex-wrap justify-start items-center gap-4">Long description of epic here. Can be Markdown in future.</div>
+					</div>
+					{/* Some Widgets */}
+
+					<div className="bg-card text-card-foreground border border-border rounded-xl container p-6">
+						<div className="flex w-full font-bold text-lg mb-4">Widget here</div>
+						<div className="flex flex-wrap justify-start items-center gap-4">Long description of epic here. Can be Markdown in future.</div>
+						<div className="flex flex-wrap justify-start items-center gap-4">Long description of epic here. Can be Markdown in future.</div>
 					</div>
 				</div>
 			</div>
