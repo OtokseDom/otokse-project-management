5eb6a9986e973f542a21c6c8f0ef99bffff46ad6
Dominic Escoto <128564959+OtokseDom@users.noreply.github.com>
2026-01-02 20:40:15 +0400

    Update(epicHelpers): Move update and delete epic functions

--- frontend/src/utils/epicHelpers.js
+++ frontend/src/utils/epicHelpers.js
@@ -1,12 +1,15 @@
 // src/utils/taskHelpers.js
 import axiosClient from "@/axios.client";
 import { API } from "@/constants/api";
+import { useToast } from "@/contexts/ToastContextProvider";
 import { useEpicStore } from "@/store/epic/epicStore";
 import { useEpicsStore } from "@/store/epics/epicsStore";
+import { useEffect } from "react";
 
 export const useEpicHelpers = () => {
 	const { setEpics, setSelectedEpic, setEpicsLoading } = useEpicsStore();
-	const { setEpic, setEpicLoading } = useEpicStore();
+	const { setEpic, setEpicLoading, setIsOpen, setUpdateData, dialogOpen, setDialogOpen, setSelectedEpicId, setHasRelation } = useEpicStore();
+	const showToast = useToast();
 
 	const fetchEpics = async () => {
 		setEpicsLoading(true);
@@ -41,8 +44,42 @@ export const useEpicHelpers = () => {
 		}
 	};
 
+	// Update epic
+
+	const handleUpdateEpic = (epic) => {
+		setTimeout(() => {
+			setIsOpen(true);
+			setUpdateData(epic);
+		}, 100);
+	};
+
+	// Delete epic
+
+	useEffect(() => {
+		if (!dialogOpen) setHasRelation(false);
+	}, [dialogOpen]);
+
+	const checkHasRelation = async (epic = {}) => {
+		setEpicsLoading(true);
+		setTimeout(() => {
+			setDialogOpen(true);
+		}, 100);
+		setSelectedEpicId(epic.id);
+		try {
+			const hasRelationResponse = await axiosClient.post(API().relation_check("epic", epic.id));
+			setHasRelation(hasRelationResponse?.data?.data?.exists);
+		} catch (e) {
+			showToast("Failed!", e.response?.data?.message, 3000, "fail");
+			if (e.message !== "Request aborted") console.error("Error fetching data:", e.message);
+		} finally {
+			setEpicsLoading(false);
+		}
+	};
+
 	return {
 		fetchEpics,
 		fetchEpic,
+		handleUpdateEpic,
+		checkHasRelation,
 	};
 };
