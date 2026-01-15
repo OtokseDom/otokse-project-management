d1505791f0e29615c63cce25c13957cfc157461d
Dominic Escoto <128564959+OtokseDom@users.noreply.github.com>
2026-01-15 19:01:32 +0400

    Update(Task index): Layout optimized

--- frontend/src/pages/Tasks/List/index.jsx
+++ frontend/src/pages/Tasks/List/index.jsx
@@ -9,8 +9,9 @@ import { useCategoriesStore } from "@/store/categories/categoriesStore";
 import { useTaskStatusesStore } from "@/store/taskStatuses/taskStatusesStore";
 import GridList from "./grid/gridList";
 import { Button } from "@/components/ui/button";
-import { CalendarDays, Kanban, Plus, Rows3, Table } from "lucide-react";
+import { CalendarDays, Filter, Kanban, Plus, Rows3, Table } from "lucide-react";
 import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import TaskForm from "../form";
 import History from "@/components/task/History";
@@ -126,94 +127,94 @@ export default function Tasks() {
 				/>
 				<div className="flex items-center justify-between">
 					<div className="flex flex-col w-full gap-2">
-						{/* Tabs */}
-						<div className="flex w-full overflow-auto p-2">
-							<Button title="Grid view" variant={view === "grid" ? "" : "ghost"} onClick={() => setView("grid")}>
-								<Rows3 size={16} /> List
-							</Button>
-							<Button title="Table view" variant={view === "list" ? "" : "ghost"} onClick={() => setView("list")}>
-								<Table size={16} /> Table
-							</Button>
-							<Button title="Calendar view" variant={view === "calendar" ? "" : "ghost"} onClick={() => setView("calendar")}>
-								<CalendarDays size={16} /> Calendar
-							</Button>
-							<Button title="Kanban view" variant={view === "kanban" ? "" : "ghost"} onClick={() => setView("kanban")}>
-								<Kanban size={16} /> Kanban Board
-							</Button>
-						</div>
-						<div className="flex w-full justify-between items-center">
-							<h1 className=" font-extrabold text-xl">Tasks</h1>
-							<Sheet open={isOpen} onOpenChange={setIsOpen} modal={false}>
-								<SheetTrigger asChild>
-									<Button variant="">
-										<Plus />
-										Add Task
+						<h1 className=" font-extrabold text-xl">Tasks</h1>
+						<div className="flex flex-col md:flex-row w-full justify-between items-start gap-4">
+							<div className="flex flex-col w-full justify-start gap-4">
+								{/* Tabs */}
+								<div className="flex w-full overflow-auto no-scrollbar">
+									<Button title="Grid view" variant={view === "grid" ? "" : "ghost"} onClick={() => setView("grid")}>
+										<Rows3 size={16} /> List
 									</Button>
-								</SheetTrigger>
-								<SheetContent side="right" className="overflow-y-auto w-full sm:w-[640px] p-2 md:p-6">
-									<SheetHeader>
-										<SheetTitle>
-											<Tabs
-												loading={tasksLoading}
-												updateData={updateData}
-												activeTab={activeTab}
-												setActiveTab={setActiveTab}
+									<Button title="Table view" variant={view === "list" ? "" : "ghost"} onClick={() => setView("list")}>
+										<Table size={16} /> Table
+									</Button>
+									<Button title="Calendar view" variant={view === "calendar" ? "" : "ghost"} onClick={() => setView("calendar")}>
+										<CalendarDays size={16} /> Calendar
+									</Button>
+									<Button title="Kanban view" variant={view === "kanban" ? "" : "ghost"} onClick={() => setView("kanban")}>
+										<Kanban size={16} /> Kanban Board
+									</Button>
+								</div>
+								{/* Project Progress Bar */}
+								<div className="w-96 max-w-full text-xs text-muted-foreground flex flex-col items-end">
+									<span>{taskProgressText}</span>
+									<Progress value={taskProgressValue} progressColor="bg-primary/50" className="h-2 w-full mt-1" />
+								</div>
+							</div>
+							<div className="flex flex-col w-fit max-w-full justify-end items-end gap-4">
+								<div className="flex flex-col gap-2 w-96 max-w-full items-end">
+									{/* <span className="text-muted-foreground font-bold">Project</span> */}
+									<Select
+										onValueChange={(value) => {
+											setSelectedProject(activeProjects.find((project) => String(project.id) === value));
+										}}
+										value={selectedProject ? String(selectedProject.id) : ""}
+									>
+										<SelectTrigger>
+											<SelectValue placeholder="Select Project" />
+										</SelectTrigger>
+										<SelectContent>
+											{Array.isArray(activeProjects) && activeProjects.length > 0 ? (
+												activeProjects.map((project) => (
+													// <SelectItem key={project.id} value={project.id}>
+													<SelectItem key={project.id} value={String(project.id)}>
+														{project.title}
+													</SelectItem>
+												))
+											) : (
+												<SelectItem disabled>No projects available</SelectItem>
+											)}
+										</SelectContent>
+									</Select>
+								</div>
+								<Sheet open={isOpen} onOpenChange={setIsOpen} modal={false}>
+									<SheetTrigger asChild>
+										<Button variant="" className="w-fit">
+											<Plus />
+											Add Task
+										</Button>
+									</SheetTrigger>
+									<SheetContent side="right" className="overflow-y-auto w-full sm:w-[640px] p-2 md:p-6">
+										<SheetHeader>
+											<SheetTitle>
+												<Tabs
+													loading={tasksLoading}
+													updateData={updateData}
+													activeTab={activeTab}
+													setActiveTab={setActiveTab}
+													parentId={parentId}
+												/>
+											</SheetTitle>
+											<SheetDescription className="sr-only">Navigate through the app using the options below.</SheetDescription>
+										</SheetHeader>
+										{activeTab == "history" ? (
+											<History selectedTaskHistory={selectedTaskHistory} />
+										) : activeTab == "relations" ? (
+											<Relations setUpdateData={setUpdateData} setParentId={setParentId} />
+										) : activeTab == "discussions" ? (
+											<TaskDiscussions taskId={updateData?.id} />
+										) : (
+											<TaskForm
 												parentId={parentId}
+												isOpen={isOpen}
+												setIsOpen={setIsOpen}
+												updateData={updateData}
+												setUpdateData={setUpdateData}
 											/>
-										</SheetTitle>
-										<SheetDescription className="sr-only">Navigate through the app using the options below.</SheetDescription>
-									</SheetHeader>
-									{activeTab == "history" ? (
-										<History selectedTaskHistory={selectedTaskHistory} />
-									) : activeTab == "relations" ? (
-										<Relations setUpdateData={setUpdateData} setParentId={setParentId} />
-									) : activeTab == "discussions" ? (
-										<TaskDiscussions taskId={updateData?.id} />
-									) : (
-										<TaskForm
-											parentId={parentId}
-											isOpen={isOpen}
-											setIsOpen={setIsOpen}
-											updateData={updateData}
-											setUpdateData={setUpdateData}
-										/>
-									)}
-								</SheetContent>
-							</Sheet>
-						</div>
-					</div>
-				</div>
-				<div className="w-full justify-between flex items-start my-4 gap-2">
-					<div className="flex flex-col md:flex-row gap-2 justify-between w-full">
-						<div className="flex flex-col gap-2 w-96 max-w-full">
-							<span className="text-muted-foreground font-bold">Project</span>
-							<Select
-								onValueChange={(value) => {
-									setSelectedProject(activeProjects.find((project) => String(project.id) === value));
-								}}
-								value={selectedProject ? String(selectedProject.id) : ""}
-							>
-								<SelectTrigger>
-									<SelectValue placeholder="Select Project" />
-								</SelectTrigger>
-								<SelectContent>
-									{Array.isArray(activeProjects) && activeProjects.length > 0 ? (
-										activeProjects.map((project) => (
-											// <SelectItem key={project.id} value={project.id}>
-											<SelectItem key={project.id} value={String(project.id)}>
-												{project.title}
-											</SelectItem>
-										))
-									) : (
-										<SelectItem disabled>No projects available</SelectItem>
-									)}
-								</SelectContent>
-							</Select>
-						</div>
-						{/* Project Progress Bar */}
-						<div className="w-96 max-w-full text-xs text-muted-foreground flex flex-col items-end">
-							<span>{taskProgressText}</span>
-							<Progress value={taskProgressValue} progressColor="bg-primary/50" className="h-2 w-full mt-1" />
+										)}
+									</SheetContent>
+								</Sheet>
+							</div>
 						</div>
 					</div>
 				</div>
